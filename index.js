621e813a5742dafef11a43e261a982838b583325
rahiyansafz <rahiyansafin@gmail.com>
2020-11-17 16:25:59 +0600

    1 file modified

--- index.js
+++ index.js
@@ -1108,3 +1108,11 @@ function middle_elements(a, b) {
 console.log(middle_elements([1, 2, 3], [1, 5, 6]));
 console.log(middle_elements([3, 3, 3], [2, 8, 0]));
 console.log(middle_elements([4, 2, 7], [2, 4, 5]));
+console.log("");
+////////////////////////////////////////////////////////////////////////////
+
+var a = 0;
+for (let i = 0; i >= 0; i++) {
+
+  
+}
